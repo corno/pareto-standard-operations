e478a32cc46e0cc15e21f84a4d7e93b28ac9415b
Corno Schraverus <corno@schraverus.com>
2025-10-05 09:11:32 +0200

    .

--- src/impure/text/serialize_backticked_string.ts
+++ src/impure/text/serialize_backticked_string.ts
@@ -3,13 +3,16 @@ import * as _ea from 'exupery-core-alg'
 
 import { $$ as op_escape_character } from "./escape_character"
 
-export const $$ = ($: string): string => '`'
+export const $$ = ($: {
+    'value': string
+    'add delimiters': boolean
+}): string => $['add delimiters'] ? '`' : ''
     + op_escape_character(
-        $,
+        $.value,
         {
             'character code': 96, // `
             'escape character code': 92, // \
 
         }
     )
-    + '`'
+    + $['add delimiters'] ? '`' : ''
