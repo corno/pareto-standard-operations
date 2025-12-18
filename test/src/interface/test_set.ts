c2c39f14b3c0302f1ab7ea24f2e0513bcb363342
Corno Schraverus <corno@schraverus.com>
2025-12-18 18:45:03 +0100

    ...

--- test/src/interface/test_set.ts
+++ test/src/interface/test_set.ts
@@ -1,6 +1,6 @@
 import * as _et from 'exupery-core-types'
 
-import * as generic from "pareto-test/dist/interface/generic"
+import * as d_generic from "pareto-test/dist/interface/generic"
 import * as d_number_scientific_notation from "pub/dist/interface/approximate_number/scientific_notation/serializer"
 import * as d_text_pad_left from "pub/dist/interface/text/pad_left/serializer"
 import * as d_integer_fractional_decimal from "pub/dist/interface/integer/fractional_decimal/serializer"
@@ -8,49 +8,49 @@ import * as d_list_split from "pub/dist/implementation/algorithms/operations/imp
 
 export type Test_Set = {
     'list': {
-        'split': _et.Dictionary<generic.Transformer_With_Parameters<string, d_list_split.Parameters, string>>
+        'split': _et.Dictionary<d_generic.Transformer_With_Parameters<string, d_list_split.Parameters, string>>
     },
     'integer': {
         'decimal': {
-            'serializer': _et.Dictionary<generic.Transformer<number, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer<number, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, number, string>>
         }
         'hexadecimal': {
-            'serializer': _et.Dictionary<generic.Transformer<number, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer<number, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, number, string>>
         }
         'binary': {
-            'serializer': _et.Dictionary<generic.Transformer<number, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer<number, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, number, string>>
         }
         'octal': {
-            'serializer': _et.Dictionary<generic.Transformer<number, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer<number, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, number, string>>
         }
         'iso_udhr': {
-            'serializer': _et.Dictionary<generic.Transformer<number, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer<number, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, number, string>>
         }
         'fractional_decimal': {
-            'serializer': _et.Dictionary<generic.Transformer_With_Parameters<number, d_integer_fractional_decimal.Parameters, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_With_Parameters<string, d_integer_fractional_decimal.Parameters, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer_With_Parameters<number, d_integer_fractional_decimal.Parameters, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_With_Parameters<string, d_integer_fractional_decimal.Parameters, number, string>>
         }
     }
     'boolean': {
         'true_false': {
-            'serializer': _et.Dictionary<generic.Transformer<boolean, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_Without_Parameters<string, boolean>>
+            'serializer': _et.Dictionary<d_generic.Transformer<boolean, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_Without_Parameters<string, boolean, string>>
         }
     }
     'approximate_number': {
         'scientific_notation': {
-            'serializer': _et.Dictionary<generic.Transformer_With_Parameters<number, d_number_scientific_notation.Parameters, string>>
-            'deserializer': _et.Dictionary<generic.Refiner_With_Parameters<string, d_number_scientific_notation.Parameters, number>>
+            'serializer': _et.Dictionary<d_generic.Transformer_With_Parameters<number, d_number_scientific_notation.Parameters, string>>
+            'deserializer': _et.Dictionary<d_generic.Refiner_With_Parameters<string, d_number_scientific_notation.Parameters, number, string>>
         }
     }
     'text': {
         'pad_left': {
-            'serializer': _et.Dictionary<generic.Transformer_With_Parameters<string, d_text_pad_left.Parameters, string>>
+            'serializer': _et.Dictionary<d_generic.Transformer_With_Parameters<string, d_text_pad_left.Parameters, string>>
         }
     }
 }
