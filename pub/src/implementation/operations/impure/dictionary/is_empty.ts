72927a442dc2805ecfce7337eff8ab0e94246b81
Corno Schraverus <corno@schraverus.com>
2025-12-26 09:10:29 +0100

    ...

--- pub/src/implementation/operations/impure/dictionary/is_empty.ts
+++ pub/src/implementation/operations/impure/dictionary/is_empty.ts
@@ -4,9 +4,5 @@ import * as _ea from 'exupery-core-alg'
 import * as signatures from "../../../../interface/signatures"
 
 export const $$ = <T>($: _et.Dictionary<T>): boolean => {
-    let is_empty = true
-    $.map(($) => {
-        is_empty = false
-    })
-    return is_empty
+    return $.__get_number_of_entries() === 0
 }
