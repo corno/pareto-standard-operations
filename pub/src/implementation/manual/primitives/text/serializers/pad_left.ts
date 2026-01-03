626c0a438fc6c94945a6906d7dfc713110c4d06a
Corno Schraverus <corno@schraverus.com>
2026-01-03 22:37:54 +0100

    ...

--- pub/src/implementation/manual/primitives/text/serializers/pad_left.ts
+++ pub/src/implementation/manual/primitives/text/serializers/pad_left.ts
@@ -1,13 +1,10 @@
 import * as _ps from 'pareto-core-serializer'
 
 import * as signatures from "../../../../../interface/signatures"
-export const $$: signatures.serializers.primitives.text.pad_left = ($, $p) => {
-    return _ps.text.build(($i) => {
-        
-        // Add padding characters if current length is less than desired length
-        for (let i =  _ps.natural.text_length($); i < $p['desired length']; i++) {
-            $i['add character']($p['pad character'])
-        }
-        
-    }) + $
-}
+export const $$: signatures.serializers.primitives.text.pad_left = ($, $p) => _ps.text.build(($i) => {
+    // Add padding characters if current length is less than desired length
+    for (let i = _ps.natural.text_length($); i < $p['desired length']; i++) {
+        $i['add character']($p['pad character'])
+    }
+    $i['add snippet']($)
+})
